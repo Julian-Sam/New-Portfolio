61a57132f1646cdab926679b62f2d1e7f3fd5582
Julian Sam <juliansam72@gmail.com>
2017-01-10 01:47:39 +0300

    Added Basic Contact Form. Fixed Underline over navigation bar. Added github and linkedin icon.

--- scripts/active_script.js
+++ scripts/active_script.js
@@ -1,12 +1,25 @@
+var NUM_SLIDES = 4;
+
 function change_active(elem) {
     // get all 'a' elements
     var a = document.getElementsByTagName("li");
     // loop through all 'a' elements
     for (i = 0; i < a.length; i++) {
         // Remove the class 'active' if it exists
-        console.log(a[i]);
         a[i].classList.remove('active');
     }
     // add 'active' classs to the element that was clicked
     elem.classList.add('active');
+}
+
+function change_active_contact(elem) {
+    // get all 'a' elements
+    var a = document.getElementsByTagName("li");
+    // loop through all 'a' elements
+    for (i = 0; i < a.length; i++) {
+        // Remove the class 'active' if it exists
+        a[i].classList.remove('active');
+    }
+    // add 'active' classs to the element that was clicked
+    a[3].classList.add('active');
 }

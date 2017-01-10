f7f48c9b23a3b7e7e20e595a1848555785598189
Julian Sam <juliansam72@gmail.com>
2017-01-10 22:31:48 +0300

    Finally fixed annoying keyboard left issue. Used double function hack

--- scripts/active_script.js
+++ scripts/active_script.js
@@ -1,4 +1,6 @@
 var NUM_SLIDES = 4;
+var first_done = 0;
+
 
 function change_active(elem) {
     // get all 'a' elements
@@ -23,3 +25,48 @@ function change_active_contact(elem) {
     // add 'active' classs to the element that was clicked
     a[3].classList.add('active');
 }
+
+
+
+
+
+
+document.onkeydown = function(e) 
+{
+    switch (e.keyCode) 
+    {
+        case 39:
+        if (first_done == 0)
+        {
+            var a = document.getElementsByClassName("links_to_slide");
+            for (i = 0; i < a.length; i++) {
+                if (a[i].classList.contains("active"))
+                {
+                    a[i].classList.remove('active');
+                    a[(i+1) % 4].classList.add('active');
+                    $.fn.fullpage.moveTo('Home', (i) % 4);
+                    first_done++;
+                    break;
+                }
+            }
+            break;
+        }
+        case 37:
+        if (first_done == 0)
+        {
+
+            var a = document.getElementsByClassName("links_to_slide");
+            for (i = 0; i < a.length; i++) {
+                if (a[i].classList.contains("active"))
+                {
+                    a[i].classList.remove('active');
+                    a[(i+3) % 4].classList.add('active');
+                    $.fn.fullpage.moveTo('Home', (i) % 4);
+                    first_done++
+                    break;
+                }
+            }
+            break;
+        }
+    }
+};
